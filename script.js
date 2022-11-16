360f89e33a5eb58b1277eba2b3efa0ee71209390
Jdobson93 <108334463+Jdobson93@users.noreply.github.com>
2022-11-16 21:00:36 +0000

    Updated JS Script
    Updated manually due to confusing the VS code GIT intergration

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 function generateRandomNumber(num) {
     // Gets # from 0 -> num - 1
     return Math.floor(Math.random() * num)
-  }
+  };
 
   const whichPokemon = {
-    pokemonName: [Pikachu, Charmander, Bulbasaur, Squirtle],
-    pokemonType: [Electric, Fire, Grass, Water],
-    Weakness: [Ground, Water, Fire, Grass]
-  }
+    pokemonName: ['Pikachu', 'Charmander', 'Bulbasaur', 'Squirtle'],
+    pokemonType: ['Electric', 'Fire', 'Grass', 'Water'],
+    Weakness: ['Ground', 'Water', 'Fire', 'Grass']
+  };
 
   let yourPokemon= []
+
+  for(let prop in whichPokemon) {
+    let optionIdx = generateRandomNumber(whichPokemon[prop].length)
+  
+    // use the object's properties to customize the message being added to personalWisdom  
+    switch(prop) {
+      case 'pokemonName':
+        yourPokemon.push(`The Pokemon you have been assigned is "${whichPokemon[prop][optionIdx]}".`)
+        break
+      case 'pokemonType':
+      yourPokemon.push(`They are a "${whichPokemon[prop][optionIdx]} type pokemon".`)
+        break
+      case 'Weakness':
+        yourPokemon.push(`They are weak against "${whichPokemon[prop][optionIdx]}".`)
+        break
+      default:
+        yourPokemon.push('There is not enough info.')
+      }
+    };
+
+    function thePokemon(pokemon) {
+      const finalPokemon = yourPokemon.join('\n')
+      console.log(finalPokemon)};
+      
+
+      thePokemon(yourPokemon);
+
 
   
